91dbd5bad6ffde1239840a804bdf70ec43905de7
Morten Højrup Kristensen <morkr@mentor-it.dk>
2020-10-03 13:16:13 +0200

    cors

--- hello.js
+++ hello.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 
+
+var cors = require('cors');
+
 app.set('port', (process.env.PORT || 5500))
 app.use(express.static(__dirname + '/public'))
+app.use(cors);
+
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
