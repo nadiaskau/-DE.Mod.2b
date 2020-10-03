8990773831399b8301a938c9157e65130113eb23
Morten Højrup Kristensen <morkr@mentor-it.dk>
2020-10-03 12:54:31 +0200

    123

--- hello.js
+++ hello.js
@@ -1,7 +1,13 @@
-#!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(5500, 'localhost');
-console.log('Server running at http://localhost:5500/');
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5500))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
